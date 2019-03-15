301b1abdaeaf7f0b2eac27539c6ed95e8c29095a
LankyDan <danknewton@hotmail.com>
2019-03-15 14:36:10 +0000

    Turn blog post index page into cards
    - Add `cover_image` to each blog post on index page
    - Turn each blog post into a card (has a border)

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
+import Img from "gatsby-image"
 
 class BlogIndex extends React.Component {
   render() {
@@ -23,22 +24,50 @@ class BlogIndex extends React.Component {
           const title = node.frontmatter.title || node.fields.slug
           return (
             <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-              <div key={node.fields.slug}>
-                <h3
+              <div
+                key={node.fields.slug}
+                style={{
+                  border: `1px solid #d6d6d6`,
+                  margin: `0.5em auto 0px`,
+                }}
+              >
+                {node.frontmatter.cover_image && (
+                  <Img
+                    fluid={node.frontmatter.cover_image.childImageSharp.fluid}
+                    style={{
+                      position: `relative`,
+                      width: `100%`,
+                      margin: `auto`,
+                      background: `transparent no-repeat center center`,
+                      backgroundSize: `cover`,
+                      zIndex: 2,
+                    }}
+                  />
+                )}
+                <div
                   style={{
-                    marginBottom: rhythm(1 / 4),
-                    color: `black`,
+                    padding: `1em`,
                   }}
                 >
-                  {title}
-                </h3>
-                <small style={{ color: `grey` }}>{node.frontmatter.date}</small>
-                <p
-                  style={{ color: `black` }}
-                  dangerouslySetInnerHTML={{
-                    __html: node.frontmatter.description || node.excerpt,
-                  }}
-                />
+                  <h3
+                    style={{
+                      marginBottom: rhythm(1 / 4),
+                      marginTop: rhythm(1 / 4),
+                      color: `black`,
+                    }}
+                  >
+                    {title}
+                  </h3>
+                  <small style={{ color: `grey` }}>
+                    {node.frontmatter.date}
+                  </small>
+                  <p
+                    style={{ color: `black` }}
+                    dangerouslySetInnerHTML={{
+                      __html: node.frontmatter.description || node.excerpt,
+                    }}
+                  />
+                </div>
               </div>
             </Link>
           )
@@ -68,6 +97,16 @@ export const pageQuery = graphql`
             date(formatString: "MMMM DD, YYYY")
             title
             description
+            cover_image {
+              childImageSharp {
+                resize(width: 1500, height: 1500) {
+                  src
+                }
+                fluid(maxWidth: 786, maxHeight: 200, cropFocus: CENTER) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
           }
         }
       }
