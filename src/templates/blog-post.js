af6790132ff985ee1946a0256d651d0dd685f565
LankyDan <danknewton@hotmail.com>
2019-03-18 21:37:59 +0000

    Display other posts at the bottom of a blog post
    - Displays as a grid on desktop screens and then stacked when window is
    thin or on mobile.
    - Currently code is pretty much duplicated from the index page.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -11,6 +11,7 @@ import Socials from "../components/socials"
 class BlogPostTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark
+    const posts = this.props.data.allMarkdownRemark.edges
     const siteTitle = this.props.data.site.siteMetadata.title
     const { previous, next } = this.props.pageContext
 
@@ -62,30 +63,116 @@ class BlogPostTemplate extends React.Component {
           }}
         />
         <Bio />
-        <ul
+        <hr
           style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
+            marginBottom: rhythm(1),
+          }}
+        />
+        <div
+          style={{
+            maxWidth: `100%`,
+            verticalAlign: `center`,
+            margin: `0 auto`,
+            float: `none`,
+            textAlign: `center`,
+            display: `block`,
           }}
         >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
+          {posts.map(({ node }) => {
+            const title = node.frontmatter.title || node.fields.slug
+            return (
+              <Link
+                style={{
+                  boxShadow: `none`,
+                  width: `500px`,
+                  display: `inline-block`,
+                  maxWidth: `94%`,
+                  padding: `10px`,
+                  verticalAlign: `top`,
+                  textAlign: `left`,
+                  float: `none`,
+                  position: `relative`,
+                  margin: `0 auto`,
+                }}
+                to={node.fields.slug}
+              >
+                <div
+                  key={node.fields.slug}
+                  style={{
+                    border: `2px solid #d6d6d6`,
+                    margin: `0.5em 0px 0px`,
+                    boxShadow: `3px 3px 0px rgba(97, 51, 128, 0.612)`,
+                    maxWidth: `100%`,
+                  }}
+                >
+                  {node.frontmatter.cover_image && (
+                    <Img
+                      fluid={node.frontmatter.cover_image.childImageSharp.fluid}
+                      style={{
+                        position: `relative`,
+                        width: `100%`,
+                        margin: `auto`,
+                        background: `transparent no-repeat center center`,
+                        backgroundSize: `cover`,
+                        zIndex: 2,
+                      }}
+                    />
+                  )}
+                  <div
+                    style={{
+                      padding: `1em`,
+                    }}
+                  >
+                    <h3
+                      style={{
+                        marginBottom: rhythm(1 / 4),
+                        marginTop: rhythm(1 / 4),
+                        color: `black`,
+                      }}
+                    >
+                      {title}
+                    </h3>
+                    <small style={{ color: `grey` }}>
+                      {node.frontmatter.date}
+                    </small>
+                    <p
+                      style={{ color: `black` }}
+                      dangerouslySetInnerHTML={{
+                        __html: node.frontmatter.description || node.excerpt,
+                      }}
+                    />
+                  </div>
+                </div>
               </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
+            )
+          })}
+        </div>
+        <div>
+          <ul
+            style={{
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `space-between`,
+              listStyle: `none`,
+              padding: 0,
+            }}
+          >
+            <li>
+              {previous && (
+                <Link to={previous.fields.slug} rel="prev">
+                  ← {previous.frontmatter.title}
+                </Link>
+              )}
+            </li>
+            <li>
+              {next && (
+                <Link to={next.fields.slug} rel="next">
+                  {next.frontmatter.title} →
+                </Link>
+              )}
+            </li>
+          </ul>
+        </div>
       </Layout>
     )
   }
@@ -117,6 +204,31 @@ export const pageQuery = graphql`
             }
             fluid(maxWidth: 786, maxHeight: 300, cropFocus: CENTER) {
               ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+            cover_image {
+              childImageSharp {
+                resize(width: 1500, height: 1500) {
+                  src
+                }
+                fluid(maxWidth: 786, maxHeight: 200, cropFocus: CENTER) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
             }
           }
         }
