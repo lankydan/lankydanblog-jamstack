35f9d555df9f7105283588c729976af183f54e3e
LankyDan <danknewton@hotmail.com>
2019-05-17 09:15:45 +0100

    Increase quality of images

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,12 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaultQuality: 80
+      }
+    },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
